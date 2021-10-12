0553130db2adf4a0e374ebf4638b3e4bffc70810
Paul S. Strauss <pss@acm.org>
2021-10-12 16:48:22 -0700

    Added matrix-printing shortcut to help debug stuff.

--- src/Handlers/ShortcutHandler.cpp
+++ src/Handlers/ShortcutHandler.cpp
@@ -30,6 +30,23 @@ static void PrintNodeBounds_(const SG::NodePtr &node, int level) {
         PrintNodeBounds_(child, level + 1);
 }
 
+// Recursive function that prints node matrices.
+static void PrintNodeMatrices_(const SG::NodePtr &node, int level,
+                               const Matrix4f &start_matrix) {
+    const std::string indent(4 * level, ' ');
+    const std::string name = node->GetName();
+    std::cout << indent << "Node ";
+    if (! name.empty())
+        std::cout << '"' << name << "\" ";
+    std::cout << node->GetModelMatrix() << "\n";
+
+    const Matrix4f combined = start_matrix * node->GetModelMatrix();
+    std::cout << indent << "   => " << combined << "\n";
+
+    for (const auto &child: node->GetChildren())
+        PrintNodeMatrices_(child, level + 1, combined);
+}
+
 // ----------------------------------------------------------------------------
 // ShortcutHandler functions.
 // ----------------------------------------------------------------------------
@@ -54,6 +71,15 @@ bool ShortcutHandler::HandleEvent(const Event &event) {
         if (event.key_string == "<Ctrl>b") {
             std::cout << "--------------------------------------------------\n";
             PrintNodeBounds_(app_.GetContext().scene->GetRootNode(), 0);
+            std::cout << "--------------------------------------------------\n";
+            return true;
+        }
+
+        // Ctrl-M: Print matrices.
+        if (event.key_string == "<Ctrl>m") {
+            std::cout << "--------------------------------------------------\n";
+            PrintNodeMatrices_(app_.GetContext().scene->GetRootNode(), 0,
+                               Matrix4f::Identity());
             std::cout << "--------------------------------------------------\n";
             return true;
         }
