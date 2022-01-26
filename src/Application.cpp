595f19c6c814796cc2ab2d3338757fcc81bb32f7
Paul S. Strauss <pss@acm.org>
2022-01-25 17:25:23 -0800

    Improved looks of FilePanel.

--- src/Application.cpp
+++ src/Application.cpp
@@ -803,10 +803,10 @@ void Application::Impl_::AddBoards_() {
 
 void Application::Impl_::ShowInitialPanel_() {
     // Show the SessionPanel.
-    //action_manager_->ApplyAction(Action::kOpenSessionPanel);
+    action_manager_->ApplyAction(Action::kOpenSessionPanel);
 
     // XXXX Can use these for testing ...
-    action_manager_->ApplyAction(Action::kOpenSettingsPanel); // XXXX
+    //action_manager_->ApplyAction(Action::kOpenSettingsPanel); // XXXX
     //panel_manager_->OpenPanel("TestPanel"); // XXXX
     //panel_manager_->OpenPanel("FilePanel"); // XXXX
 }
