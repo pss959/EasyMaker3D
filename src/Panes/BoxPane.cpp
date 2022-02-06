26db055abbb93c9bbc632658432ed1fe3d4323a1
Paul S. Strauss <pss@acm.org>
2022-02-06 12:46:58 -0800

    Deal properly with disabled panes in ContainerPane and BoxPane.

--- src/Panes/BoxPane.cpp
+++ src/Panes/BoxPane.cpp
@@ -15,31 +15,27 @@ void BoxPane::AddFields() {
 }
 
 Vector2f BoxPane::ComputeBaseSize() const {
-    // Get the base sizes of all contained panes.
+    // Get the base sizes of all enabled contained Panes.
     const auto &panes = GetPanes();
     std::vector<Vector2f> base_sizes;
     base_sizes.reserve(panes.size());
     for (const auto &pane: panes)
-        base_sizes.push_back(pane->GetBaseSize());
+        if (pane->IsEnabled())
+            base_sizes.push_back(pane->GetBaseSize());
 
     // Sum the base size in both dimensions.
+    const int layout_dim = GetOrientation() == Orientation::kVertical ? 1 : 0;
+    const int other_dim  = 1 - layout_dim;
+
     Vector2f base_size(0, 0);
-    if (GetOrientation() == Orientation::kVertical) {
-        for (auto &s: base_sizes) {
-            base_size[0] = std::max(base_size[0], s[0]);
-            base_size[1] += s[1];
-        }
-        if (! panes.empty())
-            base_size[1] += (panes.size() - 1) * spacing_;
+    for (auto &s: base_sizes) {
+        base_size[layout_dim] += s[layout_dim];
+        base_size[other_dim]   = std::max(base_size[other_dim], s[other_dim]);
     }
-    else {
-        for (auto &s: base_sizes) {
-            base_size[0] += s[0];
-            base_size[1] = std::max(base_size[1], s[1]);
-        }
-        if (! panes.empty())
-            base_size[0] += (panes.size() - 1) * spacing_;
-    }
+
+    // Add spacing and padding.
+    if (! base_sizes.empty())
+        base_size[layout_dim] += (base_sizes.size() - 1) * spacing_;
     base_size += 2 * Vector2f(padding_, padding_);
 
     return ClampSize(*this, base_size);
@@ -61,6 +57,9 @@ void BoxPane::LayOutPanes(const Vector2f &size) {
     Point2f upper_left = box_upper_left;
     const int other_dim  = 1 - dim;
     for (const auto &pane: panes) {
+        if (! pane->IsEnabled())
+            continue;
+
         const Vector2f base_pane_size = pane->GetBaseSize();
         Vector2f pane_size;
         pane_size[dim] = base_pane_size[dim];
@@ -83,17 +82,21 @@ float BoxPane::ComputeExtraSize_(int dim, float size) {
     // Sum the base sizes of the Panes in this dimension. While doing so, count
     // the number of Panes that resize in this dimension.
     const auto &panes = GetPanes();
-    float sum = 0;
-    int   resize_count = 0;
+    size_t active_count = 0;
+    size_t resize_count = 0;
+    float  sum          = 0;
     for (const auto &pane: panes) {
-        sum += pane->GetBaseSize()[dim];
-        if (Expands_(*pane, dim))
-            ++resize_count;
+        if (pane->IsEnabled()) {
+            ++active_count;
+            sum += pane->GetBaseSize()[dim];
+            if (Expands_(*pane, dim))
+                ++resize_count;
+        }
     }
     if (resize_count == 0)
         return 0;
 
-    const float spacing = (panes.size() - 1) * spacing_;
+    const float spacing = (active_count - 1) * spacing_;
     const float padding = 2 * padding_;
     return (size - (sum + spacing + padding)) / resize_count;
 }
