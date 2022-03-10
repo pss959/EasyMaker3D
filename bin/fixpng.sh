352688828253f250ed74880600821e1c1c345375
Paul S. Strauss <pss@acm.org>
2022-03-10 14:52:56 -0800

    Use consistent 128x128 size for all icons.

--- bin/fixpng.sh
+++ bin/fixpng.sh
@@ -19,5 +19,8 @@ if [[ $# -lt 1 ]] ; then print_usage ; fi
 for png_file in $*
 do
     echo "Updating $png_file"
-    convert -define colorspace:auto-grayscale=off "$png_file" "PNG32:$png_file"
+    convert \
+        -define colorspace:auto-grayscale=off \
+        -resize 128x128! \
+        "$png_file" "PNG32:$png_file"
 done
