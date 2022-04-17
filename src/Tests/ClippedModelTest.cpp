122e97760768a98edfcbe898a12dad08087dba34
Paul S. Strauss <pss@acm.org>
2022-04-17 12:13:25 -0700

    Added tests for clipping/ClippedModel. They are failing now - need to fix.

---
+++ src/Tests/ClippedModelTest.cpp
@@ -0,0 +1,34 @@
+﻿#include "Testing.h"
+#include "Models/BoxModel.h"
+#include "Models/ClippedModel.h"
+#include "SceneTestBase.h"
+
+class ClippedModelTest : public SceneTestBase {
+};
+
+TEST_F(ClippedModelTest, NoPlanes) {
+    ModelPtr box = Model::CreateModel<BoxModel>();
+    box->SetUniformScale(4);
+    box->SetTranslation(Vector3f(0, 4, 0));
+
+    ClippedModelPtr clipped = Model::CreateModel<ClippedModel>();
+    clipped->SetOriginalModel(box);
+
+    Bounds bounds = clipped->GetBounds();
+    EXPECT_EQ(Vector3f(8, 8, 8), bounds.GetSize());
+    EXPECT_EQ(Point3f(0, 4, 0),  bounds.GetCenter());
+}
+
+TEST_F(ClippedModelTest, OnePlane) {
+    ModelPtr box = Model::CreateModel<BoxModel>();
+    box->SetUniformScale(4);
+    box->SetTranslation(Vector3f(0, 4, 0));
+
+    ClippedModelPtr clipped = Model::CreateModel<ClippedModel>();
+    clipped->SetOriginalModel(box);
+    clipped->AddPlane(Plane(0, Vector3f::AxisY()));
+
+    Bounds bounds = clipped->GetBounds();
+    EXPECT_EQ(Vector3f(8, 4, 8), bounds.GetSize());
+    EXPECT_EQ(Point3f(0, 2, 0),  bounds.GetCenter());
+}
---
+++ src/Tests/MeshCombiningTest.cpp
@@ -0,0 +1,18 @@
+﻿#include "Math/MeshBuilding.h"
+#include "Math/MeshCombining.h"
+#include "Math/MeshUtils.h"
+#include "TestBase.h"
+#include "Testing.h"
+
+class MeshCombiningTest : public TestBase {
+};
+
+TEST_F(MeshCombiningTest, ClipMesh) {
+    // Build a 10x10x10 box centered on the origin and clip off the top half.
+    TriMesh box = BuildBoxMesh(Vector3f(10, 10, 10));
+    TriMesh clipped = ClipMesh(box, Plane(0, Vector3f::AxisY()));
+
+    Bounds bounds = ComputeMeshBounds(clipped);
+    EXPECT_EQ(Vector3f(10, 5, 10), bounds.GetSize());
+    EXPECT_EQ(Point3f(0, -2.5f, 0), bounds.GetCenter());
+}
