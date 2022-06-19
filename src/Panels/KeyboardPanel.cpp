4b14bd46af172831842b07ad0a3ad292c0219c53
Paul S. Strauss <pss@acm.org>
2022-06-18 19:33:14 -0700

    KeyboardPanel handles button presses and deals with shift key to show shifted text.

--- src/Panels/KeyboardPanel.cpp
+++ src/Panels/KeyboardPanel.cpp
@@ -2,10 +2,131 @@
 
 #include <vector>
 
+#include "Panes/ButtonPane.h"
+#include "Panes/ContainerPane.h"
+#include "Panes/TextPane.h"
+#include "Util/Assert.h"
+#include "Util/General.h"
+#include "Widgets/PushButtonWidget.h"
+
 void KeyboardPanel::InitInterface() {
+    //auto key_func   = [&](const ClickInfo &){};  // XXXX
+    //auto shift_func = [&](const ClickInfo &){};  // XXXX
+
+    // Create a Key_ instance for each ButtonPane and set up a click callback.
+    std::vector<ButtonPanePtr> button_panes;
+    FindButtonPanes_(GetPane(), button_panes);
+    keys_.reserve(button_panes.size());
+    for (const ButtonPanePtr &bp: button_panes) {
+        ASSERT(bp->GetPanes().size() == 1U);
+        auto tp = Util::CastToDerived<TextPane>(bp->GetPanes()[0]);
+        ASSERT(tp);
+
+        Key_ key;
+        key.button_pane  = bp;
+        key.text_pane    = tp;
+        key.text         = tp->GetText();
+        key.shifted_text = GetShiftedText_(key.text);
+
+        const size_t index = keys_.size();
+        if (key.text == "Shift") {
+            bp->GetButton().GetClicked().AddObserver(
+                this, [&, index](const ClickInfo &){
+                    ProcessShiftKey_(keys_[index]); });
+        }
+        else {
+            bp->GetButton().GetClicked().AddObserver(
+                this, [&, index](const ClickInfo &){
+                    ProcessKey_(keys_[index]); });
+        }
+
+        keys_.push_back(key);
+    }
+
+    // XXXX
+
+
     // XXXX
 }
 
 void KeyboardPanel::UpdateInterface() {
     // XXXX
+}
+
+void KeyboardPanel::FindButtonPanes_(const PanePtr &pane,
+                                     std::vector<ButtonPanePtr> &button_panes) {
+    if (ButtonPanePtr bp = Util::CastToDerived<ButtonPane>(pane))
+        button_panes.push_back(bp);
+
+    // Recurse if this is a ContainerPane.
+    if (ContainerPanePtr ctr = Util::CastToDerived<ContainerPane>(pane)) {
+        for (const auto &sub_pane: ctr->GetPanes())
+            FindButtonPanes_(sub_pane, button_panes);
+    }
+}
+
+
+void KeyboardPanel::ProcessShiftKey_(const Key_ &key) {
+    auto &but = key.button_pane->GetButton();
+    if (but.GetToggleState()) {
+        if (shift_count_++ == 0)
+            UpdateKeyText_(true);
+    }
+    else {
+        ASSERT(shift_count_ > 0);
+        if (--shift_count_ == 0)
+            UpdateKeyText_(false);
+    }
+}
+
+void KeyboardPanel::ProcessKey_(const Key_ &key) {
+    std::cerr << "XXXX ProcessKey_ '" << key.text << "'\n";
+}
+
+void KeyboardPanel::UpdateKeyText_(bool is_shifted) {
+    for (const auto &key: keys_) {
+        if (! key.shifted_text.empty())
+            key.text_pane->SetText(is_shifted ? key.shifted_text : key.text);
+    }
+}
+
+std::string KeyboardPanel::GetShiftedText_(const std::string &s) {
+    std::string ss;
+
+    // Strings with more than one character stay the same; leave an empty
+    // string to indicate this.
+
+    if (s.size() == 1U) {
+        // Single alphabetic characters can be converted to upper case.
+        if (std::isalpha(s[0]))
+            ss = Util::ToUpperCase(s);
+
+        else {
+            // Special case other single characters.
+            switch (s[0]) {
+              case '1':  ss = "!";  break;
+              case '2':  ss = "@";  break;
+              case '3':  ss = "#";  break;
+              case '4':  ss = "$";  break;
+              case '5':  ss = "%";  break;
+              case '6':  ss = "^";  break;
+              case '7':  ss = "&";  break;
+              case '8':  ss = "*";  break;
+              case '9':  ss = "(";  break;
+              case '0':  ss = ")";  break;
+              case '-':  ss = "_";  break;
+              case '=':  ss = "+";  break;
+              case '[':  ss = "{";  break;
+              case ']':  ss = "}";  break;
+              case '\\': ss = "|";  break;
+              case ';':  ss = ":";  break;
+              case '\'': ss = "\""; break;
+              case ',':  ss = "<";  break;
+              case '.':  ss = ">";  break;
+              case '/':  ss = "?";  break;
+              default: ASSERTM(false, "Unknown text: " + s);
+            }
+        }
+    }
+    return ss;
 }
--- src/Panels/KeyboardPanel.h
+++ src/Panels/KeyboardPanel.h
@@ -1,9 +1,14 @@
 #pragma once
 
+#include <string>
+#include <vector>
+
 #include "Panels/Panel.h"
 
-DECL_SHARED_PTR(BoxPane);
+DECL_SHARED_PTR(ButtonPane);
 DECL_SHARED_PTR(KeyboardPanel);
+DECL_SHARED_PTR(Pane);
+DECL_SHARED_PTR(TextPane);
 
 namespace Parser { class Registry; }
 
@@ -18,5 +23,31 @@ class KeyboardPanel : public Panel {
     virtual void UpdateInterface() override;
 
   private:
+    struct Key_ {
+        ButtonPanePtr button_pane;
+        TextPanePtr   text_pane;
+        std::string   text;
+        std::string   shifted_text;
+    };
+    std::vector<Key_> keys_;
+
+    size_t shift_count_ = 0;  ///< Number of active shift keys.
+
+    /// Recursively adds all ButtonPane instances to the vector.
+    void FindButtonPanes_(const PanePtr &pane,
+                          std::vector<ButtonPanePtr> &button_panes);
+
+    /// Button callback for shift key.
+    void ProcessShiftKey_(const Key_ &key);
+
+    /// Button callback for all other keys.
+    void ProcessKey_(const Key_ &key);
+
+    /// Updates the text on all keys for a changing shift state.
+    void UpdateKeyText_(bool is_shifted);
+
+    /// Returns the shifted version for the given key text.
+    static std::string GetShiftedText_(const std::string &s);
+
     friend class Parser::Registry;
 };
