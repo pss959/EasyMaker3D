3a79717eb8818736f281605dd9f7906cd95d4e0a
Paul S. Strauss <pss@acm.org>
2022-12-06 14:06:30 -0800

    Fixed unit test exception handling.

--- src/Tests/TestMain.cpp
+++ src/Tests/TestMain.cpp
@@ -1,7 +1,8 @@
 #include "Testing.h"
 
+#include "Util/Assert.h"
 #include "Util/General.h"
-#include "Util/StackTrace.h"
+#include "Util/String.h"
 
 int main(int argc, char **argv) {
     // Set a flag so that code can tell a test is running.
@@ -11,10 +12,14 @@ int main(int argc, char **argv) {
         ::testing::InitGoogleTest(&argc, argv);
         return RUN_ALL_TESTS();
     }
+    catch (AssertException &ex) {
+        std::cerr << "*** Caught AssertException:\n" << ex.what() << "\n"
+                  << "*** Stack trace:\n"
+                  << Util::JoinStrings(ex.GetStackTrace(), "\n") << "\n";
+        return -1;
+    }
     catch (std::exception &ex) {
-        std::cerr << "*** Caught Exception:\n" << ex.what() << "\n"
-                  << "*** Stack trace:\n";
-        Util::PrintStackTrace();
+        std::cerr << "*** Caught Exception:\n" << ex.what() << "\n";
         return -1;
     }
 }
