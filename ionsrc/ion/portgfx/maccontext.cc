d5392dbcffe8394cbbf7fe7409d5fcf0e35cf8b7
Paul S. Strauss <pss@acm.org>
2022-12-31 18:50:43 -0800

    Actually got a window to appear on Mac
    Required changing GL symbol lookup to be consistent with GLFW.
    
    Now onto shader incompatibilities.

--- ionsrc/ion/portgfx/maccontext.cc
+++ ionsrc/ion/portgfx/maccontext.cc
@@ -14,7 +14,11 @@
 #include "ion/base/staticsafedeclare.h"
 #include "ion/port/environment.h"
 #include "ion/portgfx/glcontext.h"
+
+#define GL_GLEXT_FUNCTION_POINTERS
 #include "ion/portgfx/glheaders.h"
+
+#include <CoreFoundation/CFBundle.h>
 
 // This is defined in macfuncs.mm:
 extern uintptr_t MacGetCurrentGlContextId();
@@ -26,15 +30,17 @@ namespace {
 // This class wraps a MAC context in an ion::portgfx::GlContext implementation.
 class MacContext : public GlContext {
  public:
-  explicit MacContext(uintptr_t id) : id_(id) {}
-
+  MacContext();
   ~MacContext() override {}
+
+  void WrapContext(uintptr_t id) {
+    id_ = id;
+    SetIds(CreateId(), CreateShareGroupId(), id);
+  }
 
   // GlContext implementation.
   bool IsValid() const override { return id_; }
-  void* GetProcAddress(const char* proc_name, uint32_t flags) const override {
-    return nullptr;
-  }
+  void* GetProcAddress(const char* proc_name, uint32_t flags) const override;
   void SwapBuffers() override {}
   bool MakeContextCurrentImpl() override {
     return false;
@@ -47,8 +53,28 @@ class MacContext : public GlContext {
   bool IsOwned() const override { return false; }  // Always wrapped.
 
  private:
-  uintptr_t id_;
+  uintptr_t   id_;
+  CFBundleRef framework_;
 };
+
+MacContext::MacContext() : id_(0) {
+  framework_ = CFBundleGetBundleWithIdentifier(CFSTR("com.apple.opengl"));
+}
+
+void* MacContext::GetProcAddress(const char* proc_name, uint32_t flags) const {
+  if (framework_ == nullptr) {
+    LOG(ERROR) << "Failed to get the Framework.";
+    return nullptr;
+  }
+
+  // This is adapted from GLFW so it is consistent.
+  CFStringRef sym_name = CFStringCreateWithCString(
+      kCFAllocatorDefault, proc_name, kCFStringEncodingASCII);
+  auto symbol = CFBundleGetFunctionPointerForName(framework_, sym_name);
+  CFRelease(sym_name);
+
+  return symbol;
+}
 
 }  // anonymous namespace
 
@@ -61,7 +87,8 @@ GlContextPtr GlContext::CreateGlContext(const GlContextSpec& spec) {
 // static
 GlContextPtr GlContext::CreateWrappingGlContext() {
   uintptr_t id = MacGetCurrentGlContextId();
-  base::SharedPtr<MacContext> context(new MacContext(id));
+  base::SharedPtr<MacContext> context(new MacContext);
+  context->WrapContext(id);
   return context;
 }
 
