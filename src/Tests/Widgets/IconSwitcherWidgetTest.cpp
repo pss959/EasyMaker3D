22f912fa9613fb0383d287758ed575f98ba13a48
Paul S. Strauss <pss@acm.org>
2023-08-05 16:21:31 -0700

    More widget tests.

---
+++ src/Tests/Widgets/IconSwitcherWidgetTest.cpp
@@ -0,0 +1,79 @@
+#include "Tests/Testing.h"
+#include "Tests/Widgets/WidgetTestBase.h"
+#include "Util/Assert.h"
+#include "Widgets/IconSwitcherWidget.h"
+
+class IconSwitcherWidgetTest : public WidgetTestBase {};
+
+TEST_F(IconSwitcherWidgetTest, Defaults) {
+    auto isw = CreateObject<IconSwitcherWidget>();
+    EXPECT_EQ(-1, isw->GetIndex());
+}
+
+TEST_F(IconSwitcherWidgetTest, Switch) {
+    // Create an instance with 3 children.
+    const std::string input = R"(
+IconSwitcherWidget {
+  children: [
+    IconWidget "IW0" {},
+    IconWidget "IW1" {},
+    IconWidget "IW2" {},
+  ]
+}
+)";
+    auto isw = ReadTypedItem<IconSwitcherWidget>(input);
+    EXPECT_EQ(3U, isw->GetChildren().size());
+    EXPECT_EQ(-1, isw->GetIndex());
+    EXPECT_FALSE(isw->GetChildren()[0]->IsEnabled());
+    EXPECT_FALSE(isw->GetChildren()[1]->IsEnabled());
+    EXPECT_FALSE(isw->GetChildren()[2]->IsEnabled());
+
+    isw->SetIndex(1);
+    EXPECT_EQ(1, isw->GetIndex());
+    EXPECT_FALSE(isw->GetChildren()[0]->IsEnabled());
+    EXPECT_TRUE(isw->GetChildren()[1]->IsEnabled());
+    EXPECT_FALSE(isw->GetChildren()[2]->IsEnabled());
+
+    isw->SetIndexByName("IW2");
+    EXPECT_EQ(2, isw->GetIndex());
+    EXPECT_FALSE(isw->GetChildren()[0]->IsEnabled());
+    EXPECT_FALSE(isw->GetChildren()[1]->IsEnabled());
+    EXPECT_TRUE(isw->GetChildren()[2]->IsEnabled());
+
+    isw->SetIndex(6);
+    EXPECT_EQ(-1, isw->GetIndex());
+    EXPECT_FALSE(isw->GetChildren()[0]->IsEnabled());
+    EXPECT_FALSE(isw->GetChildren()[1]->IsEnabled());
+    EXPECT_FALSE(isw->GetChildren()[2]->IsEnabled());
+
+    TEST_THROW(isw->SetIndexByName("NoSuchChild"),
+               AssertException, "No child with name");
+}
+
+TEST_F(IconSwitcherWidgetTest, FitIntoCube) {
+    // Create an instance with children of different sizes.
+    const std::string input = R"(
+IconSwitcherWidget {
+  children: [
+    IconWidget "IW0" { shapes: [ Box { size: 1 2 3 } ] },
+    IconWidget "IW1" { shapes: [ Box { size: 2 1 6 } ] },
+  ]
+}
+)";
+    auto isw = ReadTypedItem<IconSwitcherWidget>(input);
+    isw->SetIndex(1);
+    isw->FitIntoCube(12, Point3f(10, 10, 10));
+
+    // IconSwitcherWidget should be translated to center.
+    EXPECT_EQ(Vector3f(1, 1, 1),    isw->GetScale());
+    EXPECT_EQ(Vector3f(10, 10, 10), isw->GetTranslation());
+
+    // Children should be scaled to fit.
+    EXPECT_EQ(Vector3f(4, 4, 4), isw->GetChildren()[0]->GetScale());
+    EXPECT_EQ(Vector3f(2, 2, 2), isw->GetChildren()[1]->GetScale());
+    EXPECT_EQ(Vector3f(0, 0, 0), isw->GetChildren()[0]->GetTranslation());
+    EXPECT_EQ(Vector3f(0, 0, 0), isw->GetChildren()[1]->GetTranslation());
+
+    // Index should not have changed.
+    EXPECT_EQ(1, isw->GetIndex());
+}
---
+++ src/Tests/Widgets/SphereWidgetTest.cpp
@@ -0,0 +1,71 @@
+#include "Parser/Exception.h"
+#include "Tests/Testing.h"
+#include "Tests/Widgets/WidgetTestBase.h"
+#include "Widgets/SphereWidget.h"
+
+class SphereWidgetTest : public WidgetTestBase {};
+
+TEST_F(SphereWidgetTest, Defaults) {
+    auto sw = CreateObject<SphereWidget>();
+
+    EXPECT_EQ(Rotationf::Identity(), sw->GetCurrentRotation());
+}
+
+TEST_F(SphereWidgetTest, NoMotion) {
+    auto sw = CreateObject<SphereWidget>();
+
+    // The intersection point is used for the sphere radius.
+    DragTester dt(sw);
+    dt.SetRayDirection(-Vector3f::AxisZ());
+    dt.ApplyMouseDrag(Point3f(0, 0, 0), Point3f(.00001f, 0, 0));
+
+    EXPECT_EQ(Rotationf::Identity(), sw->GetCurrentRotation());
+}
+
+TEST_F(SphereWidgetTest, Drag) {
+    auto sw = CreateObject<SphereWidget>();
+
+    // The intersection point is used for the sphere radius.
+    DragTester dt(sw);
+    dt.SetRayDirection(-Vector3f::AxisZ());
+    dt.ApplyMouseDrag(Point3f(0, 0, 0), Point3f(4, 0, 0));
+
+    EXPECT_ROTS_CLOSE(BuildRotation(0, 1, 0, 207.846f),
+                      sw->GetCurrentRotation());
+}
+
+TEST_F(SphereWidgetTest, GripDrag) {
+    auto sw = CreateObject<SphereWidget>();
+
+    DragTester dt(sw);
+    dt.ApplyGripRotationDrag(Vector3f::AxisX(),
+                             BuildRotation(0, 1, 0, 20),
+                             BuildRotation(0, 1, 0, 100));
+    EXPECT_ROTS_CLOSE(BuildRotation(0, 1, 0, 80), sw->GetCurrentRotation());
+
+    // Accumulate.
+    dt.ApplyGripRotationDrag(Vector3f::AxisX(),
+                             BuildRotation(1, 0, 0, -10),
+                             BuildRotation(1, 0, 0, -100));
+    EXPECT_ROTS_CLOSE(BuildRotation(-1, 0, 0, 90), sw->GetCurrentRotation());
+}
+
+TEST_F(SphereWidgetTest, Notify) {
+    auto sw = CreateObject<SphereWidget>();
+    std::vector<Rotationf> rots;
+    sw->GetRotationChanged().AddObserver(
+        "key", [&](Widget &, const Rotationf &rot){ rots.push_back(rot); });
+
+    DragTester dt(sw);
+    dt.SetRayDirection(-Vector3f::AxisZ());
+    dt.ApplyMouseDrag(Point3f(-2, 0, 0), Point3f(2, 0, 0), 3);
+
+    // Should have notified 4 times. (4 value changes.)
+    const float deg = 207.846f;
+    EXPECT_ROTS_CLOSE(BuildRotation(0, 1, 0, deg), sw->GetCurrentRotation());
+    EXPECT_EQ(4U, rots.size());
+    EXPECT_ROTS_CLOSE(BuildRotation(0, 1, 0, .25f * deg), rots[0]);
+    EXPECT_ROTS_CLOSE(BuildRotation(0, 1, 0, .5f  * deg), rots[1]);
+    EXPECT_ROTS_CLOSE(BuildRotation(0, 1, 0, .75f * deg), rots[2]);
+    EXPECT_ROTS_CLOSE(BuildRotation(0, 1, 0,   1  * deg), rots[3]);
+}
