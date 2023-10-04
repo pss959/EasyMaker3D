e8713a256565645a0d6f6b19ba143e35a83a8e03
Paul S. Strauss <pss@acm.org>
2023-10-03 18:35:22 -0700

    Merged AppTestBase into SessionTestBase, which was now the only class derived from it.

--- src/Tests/Session/SessionTestBase.cpp
+++ src/Tests/Session/SessionTestBase.cpp
@@ -1,12 +1,59 @@
 #include "Tests/Session/SessionTestBase.h"
 
+#include "App/Application.h"
 #include "Managers/SceneContext.h"
 #include "Managers/SelectionManager.h"
 #include "Managers/SessionManager.h"
 #include "SG/Search.h"
 #include "Tests/Testing.h"
+#include "Util/Assert.h"
 #include "Util/FilePath.h"
+#include "Util/General.h"
 #include "Util/Tuning.h"
+
+// ----------------------------------------------------------------------------
+// SessionTestBase::TestingApp_ class.
+// ----------------------------------------------------------------------------
+
+/// Derived Application class that provides functions for testing via the
+/// Application's protected interface.
+///
+/// \ingroup Tests
+class SessionTestBase::TestingApp_ : public Application {
+  public:
+    void InitForTests();
+
+    // Make this available to SessionTestBase.
+    using Application::GetContext;
+};
+
+void SessionTestBase::TestingApp_::InitForTests() {
+    Application::Options options;
+    options.window_size.Set(800, 600);
+    Init(options);
+
+    // Make sure the TestContext has what it needs.
+    ASSERT(GetContext().scene_context);
+    ASSERT(GetContext().scene_context->frustum);
+}
+
+// ----------------------------------------------------------------------------
+// SessionTestBase class.
+// ----------------------------------------------------------------------------
+
+SessionTestBase::SessionTestBase() : app_(new TestingApp_) {
+    Util::app_type = Util::AppType::kUnitTest;
+}
+
+SessionTestBase::~SessionTestBase() {
+}
+
+void SessionTestBase::SetUp() {
+    app_->InitForTests();
+
+    // Copy Application::Context locally for convenience in derived classes.
+    context = app_->GetContext();
+}
 
 bool SessionTestBase::LoadSession(const Str &file_name) {
     ASSERT(context.session_manager);
--- src/Tests/Session/SessionTestBase.h
+++ src/Tests/Session/SessionTestBase.h
@@ -1,16 +1,27 @@
 #pragma once
 
+#include <memory>
 #include <string>
 
-#include "Tests/AppTestBase.h"
+#include "App/Application.h"
+#include "Tests/SceneTestBase.h"
 
 class Model;
 
 /// Base class for any test that loads an application session from a file and
 /// tests the results.
 /// \ingroup Tests
-class SessionTestBase : public AppTestBase {
+class SessionTestBase : public SceneTestBase {
  protected:
+    /// Application::Context used for the session.
+    Application::Context context;
+
+    SessionTestBase();
+    ~SessionTestBase();
+
+    /// Overrides this to initialize the Application.
+    virtual void SetUp() override;
+
     /// Loads a session from the named file (with no extension) in the
     /// "Sessions" subdirectory of the test data directory. Returns false on
     /// error.
@@ -19,4 +30,8 @@ class SessionTestBase : public AppTestBase {
     /// Convenience that selects the given Model, which must be found under the
     /// RootModel in the SceneContext.
     void SelectModel(const Model &model);
+
+  private:
+    class TestingApp_;
+    std::unique_ptr<TestingApp_> app_;
 };
