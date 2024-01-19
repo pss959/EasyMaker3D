5e11c799a76966339ca229d8de0edad03583ed95
Paul S. Strauss <pss@acm.org>
2024-01-19 15:46:46 -0800

    Fixed a problem with video buttons.

--- PublicDoc/docs/_static/js/playvideo.js
+++ PublicDoc/docs/_static/js/playvideo.js
@@ -1,24 +1,27 @@
 // Plays the video with the given ID starting at the given time in seconds.
-function PlayVideo(video, startTime) {
-    video.play();
-    video.pause();
-    video.currentTime = startTime;
-    video.play();
+// This returns a function to pass as an event listener so that the current
+// values of video and startTime are curried (instead of using the local value
+// after the last iteration of the loop in AddButtonListeners().
+function PlayVideoCB(video, startTime) {
+    return function() {
+        video.play();
+        video.pause();
+        video.currentTime = startTime;
+        video.play();
+    }
 }
 
 // Find all buttons with class "video-button" and adds a click listener for
 // them to play the video with the time set to their "startTime" attribute.
 function AddButtonListeners(document) {
-    buttons = document.getElementsByClassName("video-button");
-    for (var i = 0; i < buttons.length; ++i) {
-        var videoId   = buttons[i].dataset.id;
-        var startTime = buttons[i].dataset.startTime;
+    var buttons = document.getElementsByClassName("video-button");
+    for (let button of buttons) {
+        var videoId   = button.dataset.id;
+        var startTime = button.dataset.startTime;
         if (videoId && startTime) {
             var video = document.getElementById(videoId);
             if (video) {
-                buttons[i].addEventListener('click', e => {
-                    PlayVideo(video, startTime);
-                });
+                button.addEventListener('click', PlayVideoCB(video, startTime));
             }
             else {
                 console.log("*** No video with id:", videoId);
