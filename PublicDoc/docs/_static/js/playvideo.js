887772a07a215edde1cfd40f5599996fe9bb4b8d
Paul S. Strauss <pss@acm.org>
2024-01-19 11:44:27 -0800

    Fixed video button code to work regardless of video names. ("Selection" was causing things to fail because of jquery confusion.)

--- PublicDoc/docs/_static/js/playvideo.js
+++ PublicDoc/docs/_static/js/playvideo.js
@@ -1,21 +1,37 @@
 // Plays the video with the given ID starting at the given time in seconds.
-function PlayVideo(video, startTime){
+function PlayVideo(video, startTime) {
     video.play();
     video.pause();
     video.currentTime = startTime;
     video.play();
 }
 
-// Find all buttons with class "video-button" and have them play the video with
-// the time set to their "time" attribute.
-document.addEventListener('DOMContentLoaded', function (event) {
-    event.preventDefault();
+// Find all buttons with class "video-button" and adds a click listener for
+// them to play the video with the time set to their "startTime" attribute.
+function AddButtonListeners(document) {
     buttons = document.getElementsByClassName("video-button");
     for (var i = 0; i < buttons.length; ++i) {
-        var id        = buttons[i].dataset.id;
+        var videoId   = buttons[i].dataset.id;
         var startTime = buttons[i].dataset.startTime;
-        if (id && startTime)
-            buttons[i].setAttribute(
-                "onclick", `javascript:PlayVideo(${id}, ${startTime})`);
+        if (videoId && startTime) {
+            var video = document.getElementById(videoId);
+            if (video) {
+                buttons[i].addEventListener('click', e => {
+                    PlayVideo(video, startTime);
+                });
+            }
+            else {
+                console.log("*** No video with id:", videoId);
+            }
+        }
     }
+}
+
+// This has to be done when the document is complete. If this is done on
+// DOMContentLoaded, the buttons seem to get replaced or the listeners are
+// removed for some reason.
+document.addEventListener('readystatechange', function (event) {
+    event.preventDefault();
+    if (document.readyState == "complete")
+        AddButtonListeners(document);
 });
